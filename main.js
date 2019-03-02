1b9a71d10a5b206e6f0c1d8148b4449ddbe3b816
Nodos <migue.nodos@gmail.com>
2019-03-02 00:09:22 -0600

    portfolio

---
+++ main.js
@@ -0,0 +1,46 @@
+/*!
+	Theme Name: Migue
+	Theme URI: http://migue.design
+	Author: @elnodos
+	Author URI: http://migue.design
+	Author Contact: hello@migue.design
+	Version: 2.0
+
+	Licensed under the MIT license.
+	Copyright 2017
+*/
+
+jQuery(document).ready(function($) {
+	
+	//randomises the page title from a small handful of uninteresting options
+	var pagetitles = ["Migue", "Migue : @elnodos", "@elnodos", "Migue, hola!"];
+	var pickpagetitle = Math.floor(Math.random()*pagetitles.length);
+
+	$(document).attr('title', pagetitles[pickpagetitle]);
+
+	$('.trigger').on('click', function(e){
+		e.preventDefault();
+		
+		$this = $(this);
+		trigger = $this.attr('href');
+		
+		if($this.hasClass('is-clicked')){
+			$this.removeClass('is-clicked');
+			$("#"+trigger).removeClass('is-show');
+		}
+		else{
+			$('.trigger').removeClass('is-clicked');
+			$('.content').removeClass('is-show');
+
+			$this.addClass('is-clicked');
+			$("#"+trigger).addClass('is-show');
+		}
+
+	});
+
+});
+
+
+
+
+
