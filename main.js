9d1d0277b7fca6aa4c72f63377e1aae415520f34
Nodos <migue.nodos@gmail.com>
2019-03-03 11:37:56 -0600

    add favicon dark

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ jQuery(document).ready(function($) {
 	//randomises the page title from a small handful of uninteresting options
 	var pagetitles = ["Migue", "Migue : @elnodos", "@elnodos", "Migue, hola!"];
 	var pickpagetitle = Math.floor(Math.random()*pagetitles.length);
-
 	$(document).attr('title', pagetitles[pickpagetitle]);
 
 	$('.trigger').on('click', function(e){
