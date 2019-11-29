9d998a0a5c5ce34862b8521e2a18182d41854d3c
Migue Martínez <migue@No2.local>
2019-11-29 01:48:54 -0600

    add slider

---
+++ fluid/2/js/main.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+
+	var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        margin: 10,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      })
+      
+});
