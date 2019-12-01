813535ffe6e9f89b616a7208cfff41d7f79817d6
Migue Martínez <migue@No2.local>
2019-12-01 14:00:36 -0600

    Fluid V2

--- fluid/js/main.js
+++ fluid/js/main.js
@@ -1,171 +1,93 @@
 jQuery(document).ready(function($) {
 
-	/* Mobile
-	------------------------------*/
 
-	var isMobile = false;
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {	
-		isMobile = true;
+  // Theme  day & dark
+	function theme(){
+		user = window.localStorage.getItem('user');
+		
+		render(user);
 	}
-	/*------------------------------*/
-
-	var form = $('.Form'),
-			container = $('.Form_Container'),
-			confirm = $('.Confirm'),
-			form_title = $('.Form_Title'),
-			email = $('#mce-EMAIL');
-			Js_ShowForm = $('.Js_ShowForm'),
-			Js_SendUpdate = $('.Js_SendUpdate'),
-			Js_GetEmail = $('.Js_GetEmail');
+	theme();
 
 
-
-	Js_ShowForm.on('click',function(e){
+	$('.toggle-dark').on('click', function(e){
 		e.preventDefault();
-		$(this).addClass('is-hide');
-
-		if(isMobile){
-			form.addClass('is-show');
-			container.addClass('is-show');
-			email.focus();
-
-		}
-		else{
-			// href = $(this).attr('href', 'uploads/Fluid-UI-Library.sketch');
-			// window.location = $(this).attr('href');
-			href = './uploads/Fluid-UI-Library.sketch'
-			window.open(href);
-			confirm.addClass('is-show');
-		}
-
-
-	});
-
-
-	Js_GetEmail.on('click',function(e){
-		
-		form.addClass('is-show');
-		confirm.removeClass('is-show');
-		container.addClass('is-show');
-		form_title.html('Get updates of Fluid');
-		Js_SendUpdate.html('Subscribe to list');
-		email.focus();
-
-	});
-
-
-	var submit = $('#mc-embedded-subscribe'),
-			input = $('#mce-EMAIL'),
-			$resultElement = $('#resultElement'),
-			$form = $("#mc-embedded-subscribe-form");
-
-	/*
-	Validar que haya correo valido
-	------------------------------*/
-	submit.on('click', function(e){
-		e.preventDefault();
-		
-
-		texto = $(this).html();
-
-		$(this).html('').addClass('is-sending');
-
-
-		$form.validate();
-		if ($form.valid()) {
-			console.log('valido');
-			
-			submitSubscribeForm($form, $resultElement, texto);
-
-		}
-		else{
-			console.log('no valido');
-			$('#mce-EMAIL').focus();
-			submit.removeClass('is-sending').html(texto);
-
-		}
-		
-	});
-
-	function submitSubscribeForm($form, $resultElement, texto){
-		
-		$.ajax({
-			type: "GET",
-			url: $form.attr("action"),
-			data: $form.serialize(),
-			cache: false,
-			dataType: "jsonp",
-			jsonp: "c", // trigger MailChimp to return a JSONP response
-			contentType: "application/json; charset=utf-8",
-			error: function(error){
-				// According to jquery docs, this is never called for cross-domain JSONP requests
-			},
-			
-			success: function(data){
-				
-				submit.removeClass('is-sending').html(texto);
-
-				if (data.result != "success") {
-					var message = data.msg || "Sorry, it is not possible to subscribe. Please try again.";
-					$resultElement.addClass("is-error").fadeIn();
-
-					if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
-						message = "<p>You are already subscribed, Thanks!</p>";
-						$resultElement.removeClass("is-error").addClass("is-success").fadeIn();
-					}
-
-					$resultElement.html(message);
-
-				} else {
-					$resultElement.removeClass("is-error").addClass("is-success");
-					// lanzar el modal
-					window.location = "?r=ok";
-					
-				}
+		$(this).toggleClass('is-night');
+    $('body').toggleClass('Dark');
+    
+    if($(this).hasClass('is-night')){
+			const person = {
+				theme: "Dark"
 			}
-		});
-  }
+      window.localStorage.setItem('user', JSON.stringify(person));
+      $('.toggle-theme-img').attr('src', './img/moon.svg');
+      $('.hero-image-desk').attr('src', './img/fluid-img-light.png');
+      $('.hero-image-mobile').attr('src', './img/fluid-img-mobile-light.png');
+		}
+		else{
+      window.localStorage.removeItem('user');
+      $('.toggle-theme-img').attr('src', './img/sun.svg');
+      $('.hero-image-desk').attr('src', './img/fluid-img-dark.png');
+      $('.hero-image-mobile').attr('src', './img/fluid-img-mobile-dark.png');
+		}
 
+	})
 
-	function closeModal(){
-    $('#mce-EMAIL').val('');
-		form.removeClass('is-show');
-		container.removeClass('is-show');
-
-		$resultElement.fadeOut();
-		Js_ShowForm.removeClass('is-hide');
-		submit.removeClass('is-sending')
-
-	}
-
-	$(document)
-		.on('click','[href="#close"]',function(e){
-		    e.preventDefault();
-		    closeModal();
-		    })
-		.keydown(function(tecla){
-			if(form.hasClass('is-show')){
-		    if (tecla.keyCode == 27) { 
-			    closeModal();			    
-		    }
-		  }
-		});
-
-	function getParameterByName(name) {
-	    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-	    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-	    results = regex.exec(location.search);
-	    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-	}
-
-	var r = getParameterByName('r');
-
-	
-	if (r == 'ok') {
-
-		confirm.addClass('is-show');
-		Js_GetEmail.parent().html('<strong>You are already subscribed!</strong><br><small>You will get an email when there is an update to the Fluid library.<br>No spam,promise :)</small>');
+	function render(user){
+		user = user;
+		if(user){
+			$('.toggle-dark').toggleClass('is-night');
+			$('body').toggleClass('Dark');
+		}
 	}
 
 
+
+  // Change Default - Alt
+  $('.toggleAlt').on('click', function(e){  
+    e.preventDefault();  
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+  
+    alt = $(this).attr('href');
+    
+    if(alt == '#Default'){
+      $('.features').removeClass('is-alt');
+      $('.owl-default').addClass('is-active');
+      $('.owl-alt').removeClass('is-active').addClass('is-hide');
+    }
+    else{
+      $('.features').addClass('is-alt');
+      $('.owl-alt').addClass('is-active');
+      $('.owl-default').removeClass('is-active').addClass('is-hide');
+    }
+  });
+  
+
+
+  var owl = $('.owl-carousel');  
+  owl.owlCarousel({
+      margin: 10,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 4500,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    })
+
+
+
+
+
+      
 });
