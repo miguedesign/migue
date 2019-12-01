1faa28a223cc5b0d4473b4a445d8e5f81dd073f1
Migue Martínez <migue@No2.local>
2019-12-01 01:17:14 -0600

    dark theme

--- fluid/2/js/main.js
+++ fluid/2/js/main.js
@@ -1,5 +1,49 @@
 jQuery(document).ready(function($) {
-  
+
+
+  // Theme  day & dark
+	function theme(){
+		user = window.localStorage.getItem('user');
+		
+		render(user);
+	}
+	theme();
+
+
+	$('.toggle-dark').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('is-night');
+    $('body').toggleClass('Dark');
+    
+    if($(this).hasClass('is-night')){
+			const person = {
+				theme: "Dark"
+			}
+      window.localStorage.setItem('user', JSON.stringify(person));
+      $('.toggle-theme-img').attr('src', './img/moon.svg');
+      $('.hero-image-desk').attr('src', './img/fluid-img-light.png');
+      $('.hero-image-mobile').attr('src', './img/fluid-img-mobile-light.png');
+		}
+		else{
+      window.localStorage.removeItem('user');
+      $('.toggle-theme-img').attr('src', './img/sun.svg');
+      $('.hero-image-desk').attr('src', './img/fluid-img-dark.png');
+      $('.hero-image-mobile').attr('src', './img/fluid-img-mobile-dark.png');
+		}
+
+	})
+
+	function render(user){
+		user = user;
+		if(user){
+			$('.toggle-dark').toggleClass('is-night');
+			$('body').toggleClass('Dark');
+		}
+	}
+
+
+
+  // Change Default - Alt
   $('.toggleAlt').on('click', function(e){  
     e.preventDefault();
     $(this).addClass('is-active').siblings().removeClass('is-active');
@@ -40,5 +84,10 @@ jQuery(document).ready(function($) {
         }
       }
     })
+
+
+
+
+
       
 });
