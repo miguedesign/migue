ad042d2fe093bef79272fa17d00e422500bde265
Miguel Martinez <miguel.martinez@MC-004394-AP.local>
2021-05-27 20:53:12 -0500

    remove portfolio

---
+++ _2021.js
@@ -0,0 +1,18 @@
+/*!
+	Theme Name: Migue
+	Theme URI: http://migue.design
+	Author: @elnodos
+	Author URI: http://migue.design
+	Author Contact: hello@migue.design
+	Version: 2.5
+  Copyright ©2021 Migue Martínez
+*/
+
+jQuery(document).ready(function($) {
+	
+	//randomises the page title from a small handful of uninteresting options
+	// var pagetitles = ["Migue", "Migue : @elnodos", "@elnodos", "Migue, hola!"];
+	// var pickpagetitle = Math.floor(Math.random()*pagetitles.length);
+	// $(document).attr('title', pagetitles[pickpagetitle]);
+
+});
