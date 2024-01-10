99f67d46b56bfa515e0c399fd6cb9e4160189611
Migue Nodos <migue.nodos@gmail.com>
2024-01-09 20:48:34 -0600

    about

--- 2024.js
+++ 2024.js
@@ -13,17 +13,47 @@ jQuery(document).ready(function($) {
 	//randomises the page title from a small handful of uninteresting options
 	var pagetitles = ["Migue", "Migue : @elnodos", "@elnodos", "Migue, hola!"];
 	var pickpagetitle = Math.floor(Math.random()*pagetitles.length);
+	
 	$(document).attr('title', pagetitles[pickpagetitle]);
 
-    var $img = $('.coverDomestika'),
-        $videoDomestika = '<iframe width="560" height="315" src="https://www.youtube.com/embed/zigFVjPlSgM?si=5GcjCrUqVbDQt0l6&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+	//Random pictures by Nodos
+	var $picNodos = $('#pictureNodos'),
+		$route = '../Static/images/',
+		pictureNodos = ["migue@elnodos", "migue@elnodos2"],
+		picNodos = Math.floor(Math.random()*pictureNodos.length),
+		$srcBase = $route+pictureNodos[picNodos]+".jpg";
+	
+	$picNodos.attr("src", $srcBase);
+
+
+	//Remplace Youtube Video 
+    var $img = $('.coverDomestika');
 
     $img.on('click', function(){
-        
         $img.html("<iframe width='560' height='315' src='https://www.youtube.com/embed/zigFVjPlSgM?si=5GcjCrUqVbDQt0l6&autoplay=1&mute=1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>");
-        
         $img.addClass('isPlaying');
+	});
+
+
+	//Accordion for about
+	$('#accordion').find('.btnToggle').click(function(e){
+		e.preventDefault();
+		
+		//Expand or collapse this panel
+		var isActive = $(this).hasClass("isActive");
+		$('.btnToggle').removeClass('isActive')
+		
+		if (!isActive) {
+			$(this).toggleClass('isActive');
+		}
+
+		$(this).next().slideToggle(300);     
+		//Hide the other panels
+		$(".toggleExpand").not($(this).next()).slideUp(300);
 
 	});
+
+
 
 });
